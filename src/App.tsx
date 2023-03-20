e10f3cb3ccb37981f9353bea04a70cba71a7add3
Emmanuel Onyeyaforo <rahzy24@gmail.com>
2023-03-20 07:57:22 +0100

    init - pragma-test

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <div className='bg-green-500 h-[100vh] flex items-center justify-between'>
+    PRESSURE, oressure pressure gbas gbos, Iwan ginger you gogngo
+   </div>
+  )
+}
+
+export default App
